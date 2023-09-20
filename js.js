9708669272c18ea11e220abb84430117b514ed4e
Thung Le <lechithung@gmail.com>
2023-09-20 11:26:56 +0700

    Weather-App

---
+++ js.js
@@ -0,0 +1,51 @@
+var search = document.querySelector('.search')
+var city = document.querySelector('.city')
+var country = document.querySelector('.country')
+var value = document.querySelector('.value')
+var shortDesc = document.querySelector('.short-desc')
+var visibility = document.querySelector('.visibility span')
+var wind = document.querySelector('.wind span')
+var sun = document.querySelector('.sun span')
+var time = document.querySelector('.time')
+var content = document.querySelector('.content')
+var body = document.querySelector('body')
+
+ async function changWeatherUI(capitalSearch){
+    
+    let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=1b95f58de9e47446f695008ab8a3ea2b`
+
+    let data= await fetch(apiURL).then(res=> res.json())
+    if(data.cod==200){
+        content.classList.remove('hide')
+        city.innerText= data.name
+        country.innerText=data.sys.country
+        visibility.innerText=data.visibility + 'm'
+        wind.innerText=data.wind.speed + 'm/s'
+        sun.innerText=data.main.humidity + '%'
+        let temp=Math.round((data.main.temp -273.15))
+        value.innerText=temp
+        shortDesc.innerText=data.weather[0] ? data.weather[0].main :''
+        time.innerText=new Date().toLocaleString('vi') 
+        
+        body.setAttribute('class', 'hot')
+        if(temp<25){
+            body.setAttribute('class', 'cold')
+        }
+           
+
+    }else{
+        content.classList.add('hide')
+    }
+    
+}
+
+
+
+search.addEventListener('keypress', function(e){
+    if(e.code=='Enter'){
+        let capitalSearch = search.value.trim()
+        changWeatherUI(capitalSearch)
+    }
+})
+
+changWeatherUI('Ha Noi')
